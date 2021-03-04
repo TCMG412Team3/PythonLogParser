65097f0a8ee7277949a6dda992e4d05bb2f9a0c1
victoria-kristina <78825401+victoria-kristina@users.noreply.github.com>
2021-03-03 22:50:42 -0600

    Update LogParser.py
    Please double check that my re.search syntax is counting the correct codes (3xx vs. 4xx). thanks.

--- LogParser.py
+++ LogParser.py
@@ -1,5 +1,6 @@
 import urllib.request
 import os.path
+import re
 
 def main():
     # Check if logfile already exists locally
@@ -21,11 +22,23 @@ def main():
     # Determine total requests from the entire time period of the log
     last_year_requests = 0
     total_number_requests = 0
+    four_hundred_codes = 0
+    three_hundred_codes = 0
     
     for line in logFile:
         total_number_requests += 1
         if "/1995:" in line:
             last_year_requests += 1
+        # finds & counts 4xx status codes
+        elif re.search('\" 4[0-9]{2}', line) != None:
+            four_hundred_codes += 1
+        # finds & 3xx status codes
+        elif re.search('\" 3[0-9]{2}', line) != None:
+            three_hundred_codes += 1
+     
+    # calculates percentage of 3xx and 4xx codes 
+    four_hundred_code_percent = (four_hundred_codes / total_number_requests) * 100
+    three_hundred_code_percent = (three_hundred_codes / total_number_requests) * 100
     
     # Close the log file when we are done with it
     logFile.close()
@@ -33,6 +46,8 @@ def main():
     # Display the statitics determined above to the console
     print("Total number of requests made last year: {0}".format(last_year_requests))
     print("Total number of requests made in the whole time period: {0}".format(total_number_requests))
+    print("Percentage of requests not successful (4xx codes): {0:.3f}%".format(four_hundred_code_percent))
+    print("Percentage of requests redirected elsewhere (3xx codes): {0:.3f}%".format(three_hundred_code_percent))
     
 if __name__ == "__main__":
     main()
